5e92cbb8dace2d6e993a1681c6c4b5aac19a33d6
Adrian Yates <adeyatz@gmail.com>
2023-09-04 18:04:35 +0100

    Compass(): gettNextMovePosition passes all tests

--- src/test/java/org/melody/rover/IDirectionTest.java
+++ src/test/java/org/melody/rover/IDirectionTest.java
@@ -50,8 +50,70 @@ class IDirectionTest {
         Position expectedPosition = new CartesianPosition(0,1);
 
         assertEquals(expectedPosition, direction.getNextMovePosition(currentPosition));
-
     }
+
+
+    @Test
+    void testGetNextMovePositionBigGrid() {
+        IDirection direction = new Compass(Compass.Bearing.EAST);
+
+        Position currentPosition = new CartesianPosition(500,800);
+        Position expectedPosition = new CartesianPosition(501,800);
+
+        assertEquals(expectedPosition, direction.getNextMovePosition(currentPosition));
+    }
+
+
+
+    @Test
+    void testPositionAndRotateTestCase1() {
+        // Run the test sample 1 2 N, LMLMLMLMM
+        // 1 2 N
+        Position currentPosition = new CartesianPosition(1,2);
+        IDirection direction = new Compass(Compass.Bearing.NORTH);
+
+        String expected = "1 3 N";
+
+        // LMLMLMLMM
+        direction.rotateLeft();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        direction.rotateLeft();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        direction.rotateLeft();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        direction.rotateLeft();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        currentPosition = direction.getNextMovePosition(currentPosition);
+
+        assertEquals(expected, String.format("%s %s", currentPosition.toString(), direction.toString()));
+    }
+
+
+
+    @Test
+    void testPositionAndRotateTestCase2() {
+        // Run the test sample 3 3 E, MMRMMRMRRM
+        // 3 3 E
+        Position currentPosition = new CartesianPosition(3, 3);
+        IDirection direction = new Compass(Compass.Bearing.EAST);
+
+        String expected = "5 1 E";
+
+        //MMRMMRMRRM
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        direction.rotateRight();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        direction.rotateRight();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+        direction.rotateRight();
+        direction.rotateRight();
+        currentPosition = direction.getNextMovePosition(currentPosition);
+
+        assertEquals(expected, String.format("%s %s", currentPosition.toString(), direction.toString()));
+    }
+
 
     @Test
     void testToStringMethodNorthEastSouthWest() {
