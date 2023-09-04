0740f9b4080365080376f29c9badbffede743334
Adrian Yates <adeyatz@gmail.com>
2023-09-04 15:51:01 +0100

    CartesianPosition()  Passing Equals() method Test cases

--- src/test/java/org/melody/rover/CartesianPositionTest.java
+++ src/test/java/org/melody/rover/CartesianPositionTest.java
@@ -7,15 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class CartesianPositionTest {
 
     @Test
-    public void testCartesianPostionContructor() {
+    public void testCartesianPositionConstructor() {
 
         CartesianPosition position = new CartesianPosition(0,0);
         assertNotNull( position );
     }
 
     @Test
-    void testEquals() {
+    void testEqualsCartesianPosition() {
+        CartesianPosition p1 = new CartesianPosition(0, 0);
+        CartesianPosition p2 = new CartesianPosition( 0, 0);
+
+        assertEquals( p1, p1);  // Same object
+        assertEquals( p1, p2);  // Same coordinates
     }
+
+
+    @Test
+    void testEqualsCartesianPosition55() {
+        CartesianPosition p1 = new CartesianPosition(5, 5);
+        CartesianPosition p2 = new CartesianPosition( 5, 5);
+
+        assertEquals( p1, p2);  // Same coordinates
+    }
+
+
+    @Test
+    void testNotEqualsCartesianPosition() {
+        CartesianPosition p1 = new CartesianPosition(0, 0);
+        CartesianPosition p2 = new CartesianPosition( 1, 0);
+        CartesianPosition p3 = new CartesianPosition( 0, 1);
+
+        assertNotEquals( p1, p2);
+        assertNotEquals( p1, p3);
+        assertNotEquals( p2, p3);
+    }
+
 
     @Test
     void getAdjacentPosition() {
