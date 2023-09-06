d2161d64f4139f82321af2d15cb129ecf946cf79
Adrian Yates <adeyatz@gmail.com>
2023-09-06 19:08:23 +0100

    MarsRoverManager(): First test Pass for Constructor

---
+++ src/test/java/org/melody/rover/MarsRoverManagerTest.java
@@ -0,0 +1,29 @@
+package org.melody.rover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarsRoverManagerTest {
+
+    @Test
+    public void testMarsRoverManagerConstructor() {
+        MarsRoverManager mrm = new MarsRoverManager();
+        assertNotNull (mrm);
+    }
+
+    @Test
+    public void testCreatePlateauMethod() {
+
+    }
+
+    @Test
+    public void testCreateMarsRoverMethod() {
+
+    }
+    @Test
+    public void testMoveMarsRoverMethod() {
+
+    }
+
+}
